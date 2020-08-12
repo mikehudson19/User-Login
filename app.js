2de0b9cfc85b3af28c49d1de8c2286edf4f20415
Admin <admin@Michaels-MacBook-Pro.local>
2020-08-12 15:47:27 +0200

    Add rudimentary form validation.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : true}))
@@ -11,7 +12,6 @@ app.use(bodyParser.urlencoded({extended : true}))
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-console.log(process.env);
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 
@@ -46,14 +46,34 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    password2: req.body.password2
-  })
-  user.save();
-  res.send('Done');
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  if (!name) { 
+    console.log('Error, no name');
+    res.send('Try again.')
+  } else if (!email.includes('@')) {
+    console.log('Error, please enter valid email address');
+    res.send('Try again.')
+
+  } else if (password !== password2) {
+    console.log('Error, your passwords do not match.')
+    res.send('Try again.')
+
+  } else {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    })
+    console.log(user);
+    // user.save();
+    res.render('access');
+  }
+  
 })
 
 
